bce48ebef6687098ab6daab40209a33b9ea42548
Hrishant Rajish <124888812+Hrishaant@users.noreply.github.com>
2024-03-15 12:45:10 +0530

    deserialization error solved

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
